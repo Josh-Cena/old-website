1a85dcd493a642566feb9c773461559be20b0bc1
Josh-Cena <sidachen2003@gmail.com>
2021-03-07 08:14:26 +0800

    Fix CI bug

--- scripts/changeUrl.sh
+++ scripts/changeUrl.sh
@@ -2,9 +2,9 @@ doc_list=( $(find ./docs -mindepth 2) )
 for doc in "${doc_list[@]}"
 do
     path="{useBaseUrl(\"img/""${doc%.*}""/"
-    sed -i '' "s|\"https://mmbiz.qpic.cn/mmbiz_png/|${path}|g" "$doc"
-    sed -i '' "s|\"https://mmbiz.qpic.cn/mmbiz_jpg/|${path}|g" "$doc"
-    sed -i '' "s|/640?wx_fmt=|.|g" "$doc"
-    sed -i '' -E "s|.png\">|.png\")}>|g" "$doc"
-    sed -i '' -E "s|.jpeg\">|.jpeg\")}>|g" "$doc"
+    sed -i'' "s|\"https://mmbiz.qpic.cn/mmbiz_png/|${path}|g" "$doc"
+    sed -i'' "s|\"https://mmbiz.qpic.cn/mmbiz_jpg/|${path}|g" "$doc"
+    sed -i'' "s|/640?wx_fmt=|.|g" "$doc"
+    sed -i'' -E "s|.png\">|.png\")}>|g" "$doc"
+    sed -i'' -E "s|.jpeg\">|.jpeg\")}>|g" "$doc"
 done
