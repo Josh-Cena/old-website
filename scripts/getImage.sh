795818389cdb6de7a44096cfb9fc7709605e8344
Josh-Cena <sidachen2003@gmail.com>
2021-03-07 11:20:14 +0800

    Add vitamin.md

--- scripts/getImage.sh
+++ scripts/getImage.sh
@@ -3,11 +3,11 @@ for doc in "${doc_list[@]}"
 do
     if [ ! -d "./static/img/""${doc%.*}" ]; then
         mkdir -p "./static/img/""${doc%.*}"
-        links=( $(grep -o -P "(?<=<Pic src=\").*?(?=\">)" "$doc") )
+        links=( $(ggrep -o -P "(?<=<Pic src=\").*?(?=\">)" "$doc") )
         for link in "${links[@]}"
         do
-            name=$(echo "$link" | grep -o -P "(?<=(jpg|png)/).*(?=/640)")
-            ext=$(echo "$link" | grep -o -P "(?<=wx_fmt=).*")
+            name=$(echo "$link" | ggrep -o -P "(?<=(jpg|png)/).*(?=/640)")
+            ext=$(echo "$link" | ggrep -o -P "(?<=wx_fmt=).*")
             wget --output-document="./static/img/""${doc%.*}""/"$name"."$ext "$link"
         done
     fi
