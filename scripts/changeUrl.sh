f83695ac3384b6ea86af043f1ecf505ddaccf5c4
Josh-Cena <sidachen2003@gmail.com>
2021-03-28 20:25:13 +0800

    Add curve.md

--- scripts/changeUrl.sh
+++ scripts/changeUrl.sh
@@ -1,10 +1,10 @@
 doc_list=( $(find ./docs -mindepth 2) )
 for doc in "${doc_list[@]}"
 do
-    path="{useBaseUrl(\"img/""${doc%.*}""/"
+    path="\"/img/""${doc%.*}""/"
     sed -i '' "s|\"https://mmbiz.qpic.cn/mmbiz_png/|${path}|g" "$doc"
     sed -i '' "s|\"https://mmbiz.qpic.cn/mmbiz_jpg/|${path}|g" "$doc"
     sed -i '' "s|/640?wx_fmt=|.|g" "$doc"
-    sed -i '' -E "s|.png\">|.png\")}>|g" "$doc"
-    sed -i '' -E "s|.jpeg\">|.jpeg\")}>|g" "$doc"
+    sed -i '' -E "s|.png\">|.png\">|g" "$doc"
+    sed -i '' -E "s|.jpeg\">|.jpeg\">|g" "$doc"
 done
--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,7 @@ module.exports = {
     'Weixin official account': [
       {
         Science: ['Science/cavalieri','Science/le-chatlier','Science/derivative','Science/marathon','Science/circular','Science/bee','Science/electromagnetic','Science/gfp','Science/summation','Science/resonance','Science/dna','Science/airy','Science/ladder','Science/amino-acid','Science/vitamin'],
-        Technology: ['Technology/xor','Technology/typeset','Technology/scripting','Technology/easy-tsims'],
+        Technology: ['Technology/xor','Technology/typeset','Technology/scripting','Technology/easy-tsims','Technology/curve'],
         Humanities: ['Humanities/price','Humanities/voting','Humanities/bananafish','Humanities/law'],
         'Short stories': ['Stories/blue-pills','Stories/suicide','Stories/sun','Stories/wine','Stories/light','Stories/cat','Stories/red-pills','Stories/dream','Stories/blood'],
       }
