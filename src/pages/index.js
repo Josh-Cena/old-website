0649a0964e8ca39015e66280e031f322ef24c435
Josh-Cena <sidachen2003@gmail.com>
2021-05-03 16:53:20 +0800

    Extract data from frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,63 +21,7 @@ import {
 import Translate from "@docusaurus/Translate";
 
 import styles from "./styles.module.css";
-
-const features = [
-  {
-    title: (
-      <Translate id="front.features.feat1.title">7-year programmer</Translate>
-    ),
-    sources: {
-      light: "img/frontpage/feature1.svg",
-      dark: "img/frontpage/feature1-dark.svg",
-    },
-    description: (
-      <Translate id="front.features.feat1">
-        Fluent with both front-end and back-end programming languages.
-        Experienced with graphics language Asymptote and typesetting language
-        LaTeX.
-      </Translate>
-    ),
-  },
-  {
-    title: <Translate id="front.features.feat2.title">STEM explorer</Translate>,
-    sources: {
-      light: "img/frontpage/feature2.svg",
-      dark: "img/frontpage/feature2-dark.svg",
-    },
-    description: (
-      <Translate
-        id="front.features.feat2"
-        values={{
-          section: (
-            <code>
-              <Translate id="front.features.feat2.section">docs</Translate>
-            </code>
-          ),
-        }}
-      >
-        {
-          "Gold medalist in physics, chemistry, and biology olympiads. Did extensive research in STEM-related fields. Read more about my STEM experience in the {section}."
-        }
-      </Translate>
-    ),
-  },
-  {
-    title: <Translate id="front.features.feat3.title">Keen debater</Translate>,
-    sources: {
-      light: "img/frontpage/feature3.svg",
-      dark: "img/frontpage/feature3-dark.svg",
-    },
-    description: (
-      <Translate id="front.features.feat3">
-        Started public forum debating in 8th grade. Debated on topics including
-        gene editing, CJS, poverty alleviation, etc. Won regional &amp; national
-        championships, as well as multiple outstanding speakers. Currently doing
-        World Schools.
-      </Translate>
-    ),
-  },
-];
+import { features } from "../data/frontpageData";
 
 const slides = [
   <div className="container">
