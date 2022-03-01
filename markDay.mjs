112efd3b7e1e0f3b4d2bbcac2bcb97250d7cdf43
Joshua Chen <sidachen2003@gmail.com>
2022-03-01 13:43:24 +0800

    add experimental non-coding marking

---
+++ markDay.mjs
@@ -0,0 +1,40 @@
+import fs from 'fs-extra';
+import fetch from 'node-fetch';
+
+async function getContributions(token, username) {
+  const headers = {
+    Authorization: `bearer ${token}`,
+  };
+  const body = {
+    query: `query {
+  user(login: "${username}") {
+    name
+    contributionsCollection {
+      contributionCalendar {
+        weeks {
+          contributionDays {
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}`,
+  };
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: headers,
+  });
+  const data = await response.json();
+  return data;
+}
+
+const data = await getContributions(process.env.ACCESS_TOKEN, 'Josh-Cena');
+const day = data.data.user.contributionsCollection.contributionCalendar.weeks.at(-1).contributionDays.at(-1);
+
+// For testing purposes; to be changed to === 0 later
+if (day.contributionCount < 100) {
+  await fs.outputFile(`./static/records/${day.date}.txt`, "I didn't code today! (Test)")
+}
